9a4c43b5844e0341586dab59632a3fcf8c4b3e66
otaviomarra <otavio755@gmail.com>
2021-06-28 13:16:14 -0300

    finish add songs to playlist

--- scripts/create_playlists.py
+++ scripts/create_playlists.py
@@ -24,27 +24,9 @@ def parse_args():
                         help='The spotify app client secret')
     parser.add_argument('-r', '--replace_playlists', action='store_true',
                         help='If declared, it will try to use the same playlists previously created for this user, with this script or create new ones')
+    parser.add_argument('-l', '--lenght', nargs='?', const=150, type=int,
+                        help='Total number of songs per playlist. Default is 150')
     return vars(parser.parse_args())
-
-
-def create_new_playlists(filename='playlists', df):
-    """
-    Create the new playlists on spotify, add them to the data file and return the dict with the playlists
-    """
-    playlists = {}
-    for i in range(len(df['label'].unique())):
-        label = df['label'].unique()[i].item()
-        spotify.create_playlist(
-            name=f'k-means-cluster-0 {label}', description='k-means generated playlist. have fun')
-
-        # generate a dict with the labels and the playlist id - this will be used to add the songs later
-        playlists[label] = r.json()['id']
-
-    tempdf = load_results(filename='playlists').set_index('user_id')
-    tempdf = tempdf.append(
-        pd.DataFrame.from_dict(playlists, orient='index'))
-    save_results(filename='playlists')
-    return playlists
 
 
 if __name__ == "__main__":
@@ -84,7 +66,7 @@ if __name__ == "__main__":
         playlists = {}
         for i in range(clusters):
             label = df['label'].unique()[i].item()
-            playlists = spotify.create_playlist(
+            playlist = spotify.create_playlist(
                 name=f'k-means-cluster-0 {label}', description='k-means generated playlist. have fun')
 
             # Generate a dict with the labels and the playlist id - this will be used to add the songs later
@@ -92,7 +74,7 @@ if __name__ == "__main__":
 
     # Overwrite previously saved playlist ids from this user - It is not possible to choose which playlists to overwrite
     playlists_df = load_results(filename='playlists')
-    playlists_df = playlists_df[playlists_df['user_id'] <> spotify.user_id]
+    playlists_df = playlists_df[playlists_df['user_id'] != spotify.user_id]
     playlists_df = playlists_df.append(
         pd.DataFrame.from_dict(playlists, orient='index'), ignore_index=True)
     save_results(filename='playlists', df=playlists_df)
@@ -100,14 +82,11 @@ if __name__ == "__main__":
     # ### Adding songs to the playlists
 
     for key in playlists:
-        playlist = playlists[key]
-        # 150 songs per playlist is a good starter. Later on I'd like to also limit the number of songs from the same artist
-        tempdf = df[df['label'] == key].sample(150)
-        # break it even to post the ids in chunks (respecting the api limitation off batches of 100s)
+        tempdf = df[df['label'] == key].sample(args['lenght'])
+        # Break the ids in chunks (respecting the api limitation of batches of 100s)
         splits = ceil(len(tempdf)/100)
         chunks = np.array_split(tempdf, splits)
         # format and call the function to every chunk
         for i in range(len(chunks)):
-            songs = chunks[i]['uris'].to_json(orient='records')
-            songs = json.dumps({'uris': songs})
-            add_songs_to_playlist(songs=songs, playlist=playlist)
+            spotify.add_song_to_playlist(songs=chunks[i],
+                                         playlist=playlist[key])
